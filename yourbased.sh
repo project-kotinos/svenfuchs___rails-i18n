845aa10c29a2035064846832d749d28b74c20f3a
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 08:57:51 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# install
+bundle install --jobs=3 --retry=3
+# script
+rake
